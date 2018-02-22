0f20ecf81b0c23eb2ef26e3c1650267f1dc2586c
Salvatore Ferrucci <salvatore@algonauti.com>
2018-02-22 16:54:26 +0100

    add spark-md5

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   root: true,
+  globals: {
+    SparkMD5: true,
+  },
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'module'
--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-active-storage'
+  name: 'ember-active-storage',
+
+  included() {
+    this._super.included.apply(this, arguments);
+    this.importDependencies();
+
+  },
+
+  importDependencies() {
+    this.import(
+      {
+        development: 'node_modules/spark-md5/spark-md5.js',
+        production: 'node_modules/spark-md5/spark-md5.min.js'
+      },
+      { prepend: true }
+    );
+  }
 };
