aad41443b2338561a0c1509093c08349df1bbed4
Salvatore Ferrucci <salvatore@algonauti.com>
2018-02-23 13:49:40 +0100

    add blob model and provide factory method

---
+++ addon/model/blob.js
@@ -0,0 +1,34 @@
+import EmberObject from '@ember/object';
+import { computed, get } from '@ember/object';
+import FileChecksum from 'ember-active-storage/utils/file-checksum';
+
+export default EmberObject.extend({
+  // Default Values
+
+  file: null,
+  checksum: null,
+
+  // Single-line Computed Properties
+
+  name: computed.alias('file.name'),
+  type: computed.alias('file.type'),
+  size: computed.alias('file.size'),
+
+  toString() {
+    return `Blob: ${get(this, 'name')} with checksum ${get(this, 'checksum')}`
+  }
+
+}).reopenClass({
+
+  build(file) {
+    return new Promise((resolve, reject) => {
+      FileChecksum.MD5(file).then( (checksum) => {
+        const blob = this.create({ file: file, checksum: checksum });
+        resolve(blob);
+      }, (error) => {
+        reject(error)
+      });
+    });
+  }
+
+});
