035f24f5d86c690983947d084531c6ae2acee09a
Salvatore Ferrucci <salvatore@algonauti.com>
2018-02-23 18:34:52 +0100

    dummy: add file-upload component and test with real backend

--- tests/dummy/app/components/file-upload.js
+++ tests/dummy/app/components/file-upload.js
@@ -1,6 +1,29 @@
 import Component from '@ember/component';
+import { inject } from '@ember/service';
+import { get } from '@ember/object';
+import { isPresent } from '@ember/utils';
+import { debug } from '@ember/debug';
+
 import layout from '../templates/components/file-upload';
 
 export default Component.extend({
-  layout
+  layout,
+
+  activeStorage: inject(),
+
+  actions: {
+    upload(event) {
+      const files = event.target.files;
+
+      if (isPresent(files)) {
+        for (var i = 0; i < files.length; i++) {
+          get(this, 'activeStorage').upload(files.item(i), 'http://localhost:4200/api/attachments/upload', {
+            progress() {
+              debug("progress");
+            }
+          });
+        }
+      }
+    }
+  }
 });
