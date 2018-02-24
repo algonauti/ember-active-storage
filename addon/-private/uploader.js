220817e8f57eb2889a7b582155eadb4a016071eb
Salvatore Ferrucci <salvatore@algonauti.com>
2018-02-24 10:18:38 +0100

    move upload implementation into dedicated class

---
+++ addon/-private/uploader.js
@@ -0,0 +1,73 @@
+import EmberObject from '@ember/object';
+import { inject } from '@ember/service';
+import { task } from 'ember-concurrency';
+import { get, setProperties } from '@ember/object';
+import { debug, inspect } from '@ember/debug';
+
+var Uploader = EmberObject.extend({
+  ajax: inject(),
+
+  upload(blob, url) {
+    get(this, '_uploadTask').perform(blob, url);
+  },
+
+  _uploadTask: task(function * (blob, url) {
+    debug(`ActiveStorage: _uploadTask`)
+
+    yield this._directUpload(blob, url);
+    yield this._blobUpload(blob);
+  }),
+
+  _directUpload(blob, url) {
+    debug(`ActiveStorage: _directUpload ${blob} to url ${url}`)
+
+    return this.get('ajax').request(url, {
+      method: 'POST',
+      data: {
+        blob: {
+          filename: blob.get('name'),
+          content_type: blob.get('type'),
+          byte_size: blob.get('size'),
+          checksum: blob.get('checksum')
+        }
+      }
+    }).then( (response) => {
+      setProperties(blob, {
+        id: response.id,
+        signedId: response.signed_id,
+        key: response.key,
+        directUploadData: response.direct_upload
+      });
+    });
+  },
+
+  _blobUpload(blob) {
+    debug(`ActiveStorage: _blobUpload ${blob}`)
+
+    return this.get('ajax').request(get(blob, 'directUploadData.url'), {
+      method: 'PUT',
+      headers: get(blob, 'directUploadData.headers'),
+      processData: false,
+      contentType: get(blob, 'type'),
+      data: get(blob, 'file'),
+      xhr: () => {
+        var xhr = new window.XMLHttpRequest();
+        xhr.upload.addEventListener('progress', (event) => this._uploadRequestDidProgress(event));
+        return xhr;
+      },
+    });
+  },
+
+  _uploadRequestDidProgress(event) {
+    debug("_uploadRequestDidProgress: " + inspect(event));
+    const progress = event.loaded / event.total * 100
+    if (progress) {
+      debug("Progress: " + progress);
+    }
+  }
+
+});
+
+Uploader.toString = () => 'Uploader';
+
+export default Uploader;
--- addon/services/active-storage.js
+++ addon/services/active-storage.js
@@ -1,58 +1,20 @@
 import Service from '@ember/service';
-import { inject } from '@ember/service';
-import { task } from 'ember-concurrency';
-import { get, setProperties } from '@ember/object';
-import { debug, inspect } from '@ember/debug';
+import Mixin from '@ember/object/mixin';
+import { getOwner } from '@ember/application';
+
+import Uploader from 'ember-active-storage/-private/uploader';
 import Blob from 'ember-active-storage/model/blob';
 
 export default Service.extend({
-  ajax: inject(),
 
-  upload(file, url) {
+  upload(file, url, mixin) {
+    const uploader = Uploader.extend(Mixin.create(mixin)).create(
+      getOwner(this).ownerInjection(),
+    );
+
     Blob.build(file).then( (blob) => {
-      get(this, '_uploadTask').perform(blob, url);
+      uploader.upload(blob, url)
     });
-  },
-
-  _uploadTask: task(function * (blob, url) {
-    debug(`ActiveStorage: _uploadTask`)
-
-    yield this._directUpload(blob, url);
-    yield this._blobUpload(blob);
-  }),
-
-  _directUpload(blob, url) {
-    debug(`ActiveStorage: _directUpload ${blob} to url ${url}`)
-
-    return this.get('ajax').request(url, {
-      method: 'POST',
-      data: {
-        blob: {
-          filename: blob.get('name'),
-          content_type: blob.get('type'),
-          byte_size: blob.get('size'),
-          checksum: blob.get('checksum')
-        }
-      }
-    }).then( (response) => {
-      setProperties(blob, {
-        id: response.id,
-        signedId: response.signed_id,
-        key: response.key,
-        directUploadData: response.direct_upload
-      });
-    });
-  },
-
-  _blobUpload(blob) {
-    debug(`ActiveStorage: _blobUpload ${blob}`)
-
-    return this.get('ajax').request(get(blob, 'directUploadData.url'), {
-      method: 'PUT',
-      headers: get(blob, 'directUploadData.headers'),
-      processData: false,
-      contentType: get(blob, 'type'),
-      data: get(blob, 'file')
-    })
   }
+
 });
