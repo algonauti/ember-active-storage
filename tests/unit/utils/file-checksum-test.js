1369297c71e667d3944bfad08a32db853f6d5dc8
Gabriela Larregle <gabilarreglemail@gmail.com>
2018-03-04 19:54:52 -0300

    file-checksum util unit test

---
+++ tests/unit/utils/file-checksum-test.js
@@ -0,0 +1,20 @@
+import { module, test } from 'qunit';
+import FileChecksum from 'ember-active-storage/utils/file-checksum';
+
+module('Unit | Utility | file-checksum', function() {
+
+  const file = new File(['foo'], "foo.txt", { type: "text/plain" });
+
+  test('initializes correctly', function(assert) {
+    const fileChecksum = FileChecksum.create({ file: file });
+    assert.ok(fileChecksum);
+  });
+
+  test('checksum is generated and encoded correctly', async function(assert) {
+    const fileChecksum = FileChecksum.create({ file: file });
+    const checksum = await fileChecksum.createMD5();
+    const base64Pattern = RegExp("^[a-zA-Z0-9+/]*={0,2}$");
+    assert.ok(checksum.length % 4 === 0);
+    assert.ok(base64Pattern.test(checksum));
+  })
+});
