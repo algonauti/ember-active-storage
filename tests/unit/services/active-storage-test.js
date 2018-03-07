830d8fe8e9e1397b7fbad8fcdca51db668687415
Davide Papagni <davide@algonauti.com>
2018-03-07 01:59:39 +0100

    active-storage service unit tests

--- tests/unit/services/active-storage-test.js
+++ tests/unit/services/active-storage-test.js
@@ -1,10 +1,37 @@
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import { get } from '@ember/object';
+import fetchFile from 'dummy/tests/helpers/fetch-file';
 
-moduleFor('service:active-storage', 'Unit | Service | active storage', {
-  needs: ['service:ajax']
-});
+module('Unit | Service | active-storage', function(hooks) {
+  setupTest(hooks);
 
-test('service#upload', function(assert) {
-  let service = this.subject();
-  assert.ok(service);
+  test('upload() returns blob model', async function(assert) {
+    let service = this.owner.lookup('service:active-storage');
+    let file = await fetchFile('/sample.pdf');
+    let blob = await service.upload(file, '/api/attachments/upload');
+    assert.equal(get(blob, 'name'), 'sample.pdf');
+    assert.equal(get(blob, 'type'), 'application/pdf');
+    assert.equal(get(blob, 'size'), 6081);
+    assert.equal(get(blob, 'id'), 123);
+    assert.ok(get(blob, 'signedId'));
+    assert.ok(get(blob, 'key'));
+    assert.equal(get(blob, 'directUploadData.url'), '/api/attachments/direct-upload');
+    assert.equal(get(blob, 'directUploadData.headers')['Content-Type'], 'application/pdf');
+  });
+
+  test('upload() invokes onProgress callback', async function(assert) {
+    let service = this.owner.lookup('service:active-storage');
+    let file = await fetchFile('/sample.pdf');
+    let n = 0;
+    await service.upload(file, '/api/attachments/upload', {
+      onProgress(progress) {
+        n++;
+        assert.ok(progress > 0);
+        assert.ok(progress <= 100);
+      }
+    });
+    assert.ok(n > 0);
+  });
+
 });
