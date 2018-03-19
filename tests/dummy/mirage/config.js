f119b76bda0c5617fea2067fd937ed58d50ff291
Davide Papagni <davide@algonauti.com>
2018-03-19 14:31:41 +0100

    uploadResponse utility function for tests

--- tests/dummy/mirage/config.js
+++ tests/dummy/mirage/config.js
@@ -1,31 +1,14 @@
 import Response from 'ember-cli-mirage/response';
+import { uploadResponse } from 'ember-active-storage/utils/tests';
 
 export default function() {
   this.namespace = '/api';
 
   this.post('/attachments/upload', (_, request) => {
-    const blob = JSON.parse(request.requestBody).blob;
-    const respHeaders = {
-      'Content-Type': 'application/json; charset=utf-8'
-    };
-    const respBody = {
-      id: 123,
-      key: 'cwUyfscVbcMNdo26Fkn9uHrW',
-      filename: blob.filename,
-      content_type: blob.content_type,
-      metadata: {},
-      byte_size: blob.byte_size,
-      checksum: blob.checksum,
-      created_at: new Date().toISOString(),
-      signed_id: 'eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--d4c77635d82e4b007598f79bc3f785854eac27b9',
-      direct_upload: {
-        url: '/api/attachments/direct-upload',
-        headers: {
-          'Content-Type': blob.content_type
-        }
-      }
-    };
-    return new Response(200, respHeaders, respBody);
+    const response = uploadResponse(request.requestBody, {
+      directUploadURL: '/api/attachments/direct-upload'
+    });
+    return new Response(200, response.headers, response.body);
   });
 
   this.put('/attachments/direct-upload', () => {
