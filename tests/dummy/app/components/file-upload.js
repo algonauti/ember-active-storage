63868ed2fcbd316cf61639c85e1a73dcd5ebf6b5
Sinan Keskin <sinan@kesk.in>
2021-02-16 22:59:36 +0300

    Fix dummy app lint errors

--- tests/dummy/app/components/file-upload.js
+++ tests/dummy/app/components/file-upload.js
@@ -1,31 +1,29 @@
-import Component from '@ember/component';
-import { inject as service } from '@ember/service';
-import { get } from '@ember/object';
-import { isPresent } from '@ember/utils';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
 import { debug } from '@ember/debug';
+import { isPresent } from '@ember/utils';
+import { inject as service } from '@ember/service';
 
-import layout from '../templates/components/file-upload';
+export default class FileUploadComponent extends Component {
+  @service
+  activeStorage;
 
-export default Component.extend({
-  layout,
+  @action
+  upload(event) {
+    const files = event.target.files;
 
-  activeStorage: service(),
-
-  actions: {
-    upload(event) {
-      const files = event.target.files;
-
-      if (isPresent(files)) {
-        for (var i = 0; i < files.length; i++) {
-          get(this, 'activeStorage').upload(files.item(i), '/api/attachments/upload', {
+    if (isPresent(files)) {
+      for (var i = 0; i < files.length; i++) {
+        this.activeStorage
+          .upload(files.item(i), '/api/attachments/upload', {
             onProgress(progress) {
-              debug("onProgress: " + progress);
-            }
-          }).then( (blob) => {
+              debug('onProgress: ' + progress);
+            },
+          })
+          .then((blob) => {
             debug(`file upload completed ${blob.get('signedId')}`);
           });
-        }
       }
     }
   }
-});
+}
